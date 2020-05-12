5de31d365f3144f33f4fae8360ab7d675d739ac9
Pablo Becerra <pablo.d.becerra@outlook.com>
2020-05-12 16:33:57 -0400

    Added new solution

---
+++ BabyTracker/Migrations/20200512195238_test.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BabyTracker.Migrations
+{
+    public partial class test : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DiaperChange",
+                table: "DiaperChange");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NumberofDiaperChanges",
+                table: "DiaperChange",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int")
+                .OldAnnotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddColumn<int>(
+                name: "DiaperChangeID",
+                table: "DiaperChange",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DiaperChange",
+                table: "DiaperChange",
+                column: "DiaperChangeID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_DiaperChange",
+                table: "DiaperChange");
+
+            migrationBuilder.DropColumn(
+                name: "DiaperChangeID",
+                table: "DiaperChange");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "NumberofDiaperChanges",
+                table: "DiaperChange",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int))
+                .Annotation("SqlServer:Identity", "1, 1");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_DiaperChange",
+                table: "DiaperChange",
+                column: "NumberofDiaperChanges");
+        }
+    }
+}
---
+++ BabyTracker/Migrations/20200512195825_unwantedColumnCleanup.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BabyTracker.Migrations
+{
+    public partial class unwantedColumnCleanup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+           
+
+            migrationBuilder.DropColumn(
+                name: "DiaperChangeID",
+                table: "DiaperChange");
+
+        
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+           
+
+        }
+    }
+}
