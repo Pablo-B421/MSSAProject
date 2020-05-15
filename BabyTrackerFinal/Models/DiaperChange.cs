6348122ef28843501c0d5deba6efd25c60251486
Pablo Becerra <pablo.d.becerra@outlook.com>
2020-05-15 08:47:15 -0400

    LAST REBUILD

---
+++ BabyTrackerFinal/Models/DiaperChange.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BabyTrackerFinal.Models
+{
+    public class DiaperChange
+    {
+        [Key]
+        public int DiaperChangeID { get; set; }
+        public int NumberOfDiaperChanges { get; set; }
+
+        public int BabyName { get; set; }
+
+        [ForeignKey("BabyName")]
+        public BabyInfo BabyInfo { get; set; }
+    }
+}
---
+++ BabyTrackerFinal/Models/Feeding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace BabyTrackerFinal.Models
+{
+    public class Feeding
+    {
+        [Key]
+        public int FeedingID { get; set; }
+        public int NumberOfFeedings { get; set; }
+
+        [DataType(DataType.DateTime)]
+        public DateTime FeedingTime { get; set; }
+
+        public int BabyName { get; set; }
+
+        [ForeignKey("BabyName")]
+        public BabyInfo BabyInfo { get; set; }
+    }
+}
